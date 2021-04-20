70489e0350f3da53c0e22a8f2afa451f29f74bdd
nonuabi <nonuabi@gmail.com>
2021-04-20 14:50:28 +0530

    created home page look and also style it, create a templete engine, now i'm able to access my static files , and the home page is dynamic now.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
+app.use(express.static("assests"));
+
+app.get("/", function (req, res) {
+  return res.render("home");
+});
 
 app.listen(port, function (err) {
   if (err) {
